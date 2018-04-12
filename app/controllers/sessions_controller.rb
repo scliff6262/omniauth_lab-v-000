383056cf8c9710de28efaf41e2be332040daaa68
Sean Clifford <sclifford6262@gmail.com>
2018-04-12 21:03:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     fb_name = request.env["omniauth.auth"]["info"]["name"]
     fb_email = request.env["omniauth.auth"]["info"]["email"]
     fb_uid = request.env["omniauth.auth"]["uid"]
-    user = User.find_or_create_by_fb_email(fb_email, fb_uid)
+    user = User.find_or_create_by_fb_email(fb_name, fb_email, fb_uid)
     session[:user_id] = user.id
   end
 
