34fcc8e8543c060a65311b85f059749f159c8bae
Sean Clifford <sclifford6262@gmail.com>
2018-04-12 20:52:51 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   def self.find_or_create_by_fb_email(fb_email, fb_uid)
     user = self.find_by(email: fb_email)
     if user
-      binding.pry
       return user
     else
+      binding.pry
       user = self.create(email: fb_email, uid: fb_uid)
     end
   end
